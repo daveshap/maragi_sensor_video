93a2d781b11e98129340145b541fd58a4c2a4496
David Shapiro <daveshap37@gmail.com>
2018-02-28 11:35:34 -0500

    MVP

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
-#!/usr/bin/env python
-from importlib import import_module
-import os
-from flask import Flask, render_template, Response
+import cv2
+from flask import Flask
+import json
 
-# import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
+cam = cv2.VideoCapture(0)
 
-# Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
+
+def snapshot():
+    s, img = cam.read()
+    return json.dumps(img.tolist(), separators=(',', ':'))
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
+@app.route("/")
+def default():
+    return str(snapshot())
 
 
-def gen(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+@app.route("/left")
+def default():
+    return str(snapshot())
 
 
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/right")
+def default():
+    return str(snapshot())
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+if __name__ == "__main__":
+    app.run()

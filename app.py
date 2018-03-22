f8eaaf3e075b63be82efabaedb075e0519a6b59e
benjaminharper <daveshap37@gmail.com>
2018-03-22 15:42:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import time
 import cv2
 from flask import Flask
 import json
 import sys
 
-cam = cv2.VideoCapture(0)
-
-
-def snapshot():
-    s, img = cam.read()
-    return json.dumps(img.tolist(), separators=(',', ':'))
+try:
+    cam0 = cv2.VideoCapture(0)
+except:
+    exit('unable to load a camera')
+try:
+    cam1 = cv2.VideoCapture(1)
+except:
+    print('unable to load second camera')
 
 
 app = Flask(__name__)
@@ -16,12 +19,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def default():
-    return str(snapshot())
+    try:
+        s, img = cam0.read()
+        serialized = str(json.dumps(img.tolist(), separators=(',', ':')))
+        return {'time': time.time(), 'img0': serialized}
+    except:
+        return 'error'
 
-
+    
 @app.route("/stereo")
 def default():
-    return str(snapshot())
+    try:
+        s, img0 = cam0.read()
+        s, img1 = cam1.read()
+        serialized0 = str(json.dumps(img0.tolist(), separators=(',', ':')))
+        serialized1 = str(json.dumps(img1.tolist(), separators=(',', ':')))
+        return {'time': time.time(), 'img0': serialized0, 'img1': serialized1}
+    except:
+        return 'error'
 
 
 if __name__ == "__main__":
@@ -29,4 +44,3 @@ if __name__ == "__main__":
         app.run(port = sys.argv[1])
     else:
         app.run(port=6000)
-    #app.run()
